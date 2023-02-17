c2802d64d77d5a0df4ed493c1401e23c91468455
Leo Singer <leo.singer@ligo.org>
2023-02-17 01:36:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Architect functions for OpenSearch / ElasticSearch
+
+This package provides an automatically configured [client for OpenSearch](https://opensearch.org/docs/latest/clients/javascript/index/) in an [Architect](https://arc.codes/) application.
+
+Pair this package with [@nasa-gcn/architect-plugins-search](https://github.com/nasa-gcn/architect-plugins-search), a [plugin](https://arc.codes/docs/en/guides/plugins/overview) that adds [Amazon OpenSearch Serverless](https://docs.aws.amazon.com/opensearch-service/latest/developerguide/serverless.html) to your application.
---
+++ index.ts
@@ -0,0 +1,38 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+import { services } from '@architect/functions'
+import { Client } from '@opensearch-project/opensearch'
+import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws'
+import memoizee from 'memoizee'
+
+export const search = memoizee(
+  async () => {
+    const discoveredServices = await services()
+    const node =
+      discoveredServices['nasa-gcn']?.['architect-plugin-search']?.node
+    if (!node) throw new Error('unknown endpoint')
+
+    const options = { node }
+
+    if (new URL(node).hostname === 'localhost') {
+      Object.assign(
+        options,
+        AwsSigv4Signer({
+          region: process.env.AWS_REGION!,
+          // @ts-expect-error: service is missing from type definition;
+          // fixed in https://github.com/opensearch-project/opensearch-js/pull/377
+          service: 'aoss',
+        })
+      )
+    }
+
+    return new Client(options)
+  },
+  { promise: true }
+)
